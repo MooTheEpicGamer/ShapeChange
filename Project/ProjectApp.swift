e461d2ba88794eedbc08f48f2e8f485b6c437793
moncheeta <moncheeta@duck.com>
2023-09-08 09:59:16 -0500

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Damian Myrda on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
