8573949524ca62db676fe55573d3854a62392455
Ryan Rasavong <ryanrasavong@icloud.com>
2023-09-12 11:10:27 -0500

    Initial Commit

---
+++ ShapeChangeFR/ShapeChangeFRApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeChangeFRApp.swift
+//  ShapeChangeFR
+//
+//  Created by Ryan S. Rasavong on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ShapeChangeFRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
